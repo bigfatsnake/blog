7dd6c15f33d5916bc33effc1f44f8f33398e7a3c
Amaya <amayazj@163.com>
2020-01-11 06:58:21 +0800

    post

--- source/_posts/jaoben.js
+++ source/_posts/jaoben.js
@@ -6,7 +6,7 @@ const Path = resolve(__dirname, './辨析词汇.md');
 let text = fs.readFileSync(Path, { encoding: 'utf-8' });
 
 text = text.replace(/(\d+?)\./g, (a, b, c, d, e, f) => {
-    if (b > 332) {
+    if (b > 406) {
 
         return (b - 1) + '.';
     } else {
