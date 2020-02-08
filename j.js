e149bbe40f24d994e53494afa6a1904e070fd42c
Amaya <amayazj@163.com>
2020-02-08 21:11:25 +0800

    post

--- j.js
+++ j.js
@@ -7,14 +7,14 @@ const { readFileSync, writeFileSync } = require('fs');
 const { resolve } = require('path');
 
 // 这里是文件路径，改不同的文件修改这里的文件名即可
-const filepath = resolve(__dirname, 'source/_posts/实词辨析.md');
+const filepath = resolve(__dirname, 'source/_posts/成语辨析.md');
 let text = readFileSync(filepath, { encoding: 'utf8' });
 
 text = text.split(/\n/).map(n => {
     if (/^\d+\.[ ]/.test(n)) {
         return n.replace(/^(\d+)\.[ ]/, (a, b, c, d) => {
             // 数字判断
-            if (b >656) {
+            if (b >456) {
                 // 这里可能会+1也有可能是-1，根据需求
                 return `${Number(b) + 1}. `;
             } else {
